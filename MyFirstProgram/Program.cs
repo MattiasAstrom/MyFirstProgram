7729120019022595f5ce6995a02f96874f961387
Mattias.Astrom <mattias.astrom@hotmail.com>
2024-09-16 15:07:19 +0200

    Add project files.

---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,32 @@
+﻿//Author; Mattias Åström NET24.
+
+int anInt = 5;
+
+//Compares the value on an int.
+if (anInt > 10)
+{
+    Console.WriteLine("Talet är stort!");
+}
+else
+{
+    Console.WriteLine("Oj. Lågt tal!");
+}
+
+//Ask user for their name and greets them (no error handling).
+Console.WriteLine("Vad heter du?");
+
+string userInput = "";
+
+userInput = Console.ReadLine();
+
+Console.WriteLine($"Hej {userInput}");
+
+
+//prints out all the numbers between 0 and the variable anInt.
+int startNumber = 0;
+
+for (int i = 0; i <= anInt; i++)
+{
+    Console.WriteLine(startNumber);
+    startNumber++;
+}
